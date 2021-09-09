46de6390777e4677e03411365fd24e09cf476aeb
Andre <andredelima92@gmail.com>
2021-09-08 22:23:44 -0300

    Controller and Service for Jogadores

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+  constructor(private readonly jogadoresService: JogadoresService) {}
+
   @Post()
-  async criarAtualizarJogador() {
-    return {
-      name: 'Andre',
-    };
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    return this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
   }
 }
---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(
+    criarJogadorDto: CriarJogadorDto,
+  ): Promise<Jogador> {
+    return this.criar(criarJogadorDto);
+  }
+
+  private criar(criarJogadorDto: CriarJogadorDto): Jogador {
+    const { email, nome, telefoneCelular } = criarJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuid(),
+      nome,
+      email,
+      telefoneCelular,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador: 'https://www.google.com.br',
+    };
+
+    this.jogadores.push(jogador);
+
+    this.logger.log(`CriaJogadorDto: ${JSON.stringify(criarJogadorDto)}`);
+
+    return jogador;
+  }
+}
